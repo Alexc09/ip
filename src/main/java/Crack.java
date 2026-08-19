7c4a53ef7e389f5daa0182977000dc88cffc5e58
Alex <alexchien09@gmail.com>
2026-08-19 10:52:41 +0800

    feat: L2 - add and list tasks

--- src/main/java/Crack.java
+++ src/main/java/Crack.java
@@ -7,11 +7,17 @@ public class Crack {
     /** Horizontal rule used to separate the chatbot's messages from each other. */
     private static final String DIVIDER = "_".repeat(60);
 
+    /** Maximum number of tasks the chatbot can hold. */
+    private static final int MAX_TASKS = 100;
+
     private static final String BANNER = "  ____                _    \n"
             + " / ___|_ __ __ _  ___| | __\n"
             + "| |   | '__/ _` |/ __| |/ /\n"
             + "| |___| | | (_| | (__|   < \n"
             + " \\____|_|  \\__,_|\\___|_|\\_\\";
+
+    private static final String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         System.out.println(DIVIDER);
@@ -22,15 +28,32 @@ public class Crack {
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             System.out.println(DIVIDER);
             if (input.equals("bye")) {
-                System.out.println("Aight bet, imma fade");
+                System.out.println("Aight bet, I'm finna fade.");
                 System.out.println(DIVIDER);
                 break;
+            } else if (input.equals("list")) {
+                listTasks();
+            } else {
+                addTask(input);
             }
-            System.out.println(input);
             System.out.println(DIVIDER);
+        }
+    }
+
+    /** Stores the given task and confirms it to the user. */
+    private static void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println("bet, added: " + task);
+    }
+
+    /** Prints every stored task, numbered from 1. */
+    private static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
         }
     }
 }
